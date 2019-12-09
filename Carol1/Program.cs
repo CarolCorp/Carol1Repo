0281f27e149a931a218c011fac0bc056f0d6b10d
Laurence Beales <labeales@btinternet.com>
2019-12-09 07:15:59 +0000

    Add project files.

---
+++ Carol1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Carol1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
